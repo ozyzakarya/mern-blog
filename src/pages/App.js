823319eee2e07ef2e004f610e903e18abb35691f
ozyzakarya <ozyzakarya@gmail.com>
2020-08-28 19:57:15 +0700

    react-router

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../assets/images/logo.svg';
+import './App.css';
+import { Routes } from '../config';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
